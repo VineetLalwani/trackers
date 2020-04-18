ae8dcbd133553777b08f8f97428834e09808923f
VineetLalwani <vineetlalwani0@gmail.com>
2020-04-18 21:15:22 +0530

    deploy my react app to github pages

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,33 @@
+import React, { useState,useEffect } from 'react';
+import { NativeSelect, FormControl, StylesProvider } from '@material-ui/core';
+
+
+import styles from './CountryPicker.module.css';
+
+import { fetchCountries } from '../../api';
+
+const CountryPicker = ( { handleCountryChange }) => {
+
+    const [fetchedCountries, setFetchedCountries ] = useState([]);
+
+    useEffect(()=> {
+        const fetchAPI = async () => {
+            setFetchedCountries (await fetchCountries());
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+
+    //console.log(fetchedCountries);
+
+    return(
+        <FormControl className={StylesProvider.formControl}>
+            <NativeSelect defaultValue="India" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value= "">GloBal</option>
+                {fetchedCountries.map((country, i) => <option key={i} value= { country }>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
